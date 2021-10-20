1598a35cc6d90c0da076b3018f5240c17d70ee0d
Rafael Nunes <67107778+rafaelnunes22@users.noreply.github.com>
2021-10-20 00:39:38 -0300

    first commit

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.scss";
+import { VscGithubInverted } from "react-icons/vsc";
+import { useContext, useEffect } from "react";
+import { api } from "../../services/api";
+import { AuthContext } from "../../contexts/auth";
+
+
+
+export function LoginBox() {
+  const { signInUrl, user } = useContext(AuthContext)
+  console.log(user)
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size={24} />
+        Entrar com o Github
+      </a>
+    </div>)
+}

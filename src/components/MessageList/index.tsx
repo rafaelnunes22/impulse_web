b9582bd7360ba5ce9bf721d2c2616e44eff832e6
unknown <rafael.nunes@meta.com.br>
2021-10-21 00:25:49 -0300

    feat(SendMessage): create sendMessageForm and use socket io to list messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,5 @@
 import { api } from "../../services/api";
-
+import io from "socket.io-client";
 import styles from "./styles.module.scss";
 
 import logoImg from "../../assets/logo.svg"
@@ -14,18 +14,41 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io("http://localhost:4000")
+
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+  console.log(newMessage)
+})
+
 export function MessageList() {
   //research about imutability
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => {
+          return [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        });
+
+        messagesQueue.shift()
+      }
+    }, 3000)
+  }, [])
+
+  useEffect(() => {
     api.get<Message[]>("messages/last3").then(response => setMessages(response.data))
   }, [])
+
 
   return <div className={styles.messageListWrapper}>
     <img src={logoImg} alt="DoWhile 2021" />
     <ul className={styles.messageList}>
-      {messages.map(message => {
+      {messages.map((message: Message) => {
+        console.log(message)
         return (
           <li key={message.id} className={styles.message}>
             <p className={styles.messageContent}>{message.text}</p>
